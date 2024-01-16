f6f47946578377b58f58b5cbd2c7bbf52822807a
Bohdan Hulobov <bogdan.gulobov@gmail.com>
2024-01-16 14:36:46 +0200

    pom update

--- pom/pages/profile.page.js
+++ pom/pages/profile.page.js
@@ -2,15 +2,11 @@ export class ProfilePage {
   constructor(page) {
     this.page = page;
     this.profileButton = page.locator('a.btn.-profile[href="/panel/profile"]');
-    this.editProfileButton = page.getByRole("button", { name: "Edit profile" });
+    this.editProfileButton = page.getByRole("button", {
+      name: "Edit profile",
+    });
     this.photoInput = page.locator('input[name="photo"]');
     this.saveButton = page.getByRole("button", { name: "Save" });
     this.successPopup = page.locator(".alert-success");
-
-    //add a car modal
-    this.brandDropdown = page.getByLabel("Brand");
-    this.modelDropdown = page.getByLabel("Model");
-    this.mileageInput = page.getByLabel("Mileage");
-    this.addButton = page.getByRole("button", { name: "Add" });
   }
 }
--- tests/e2e/profilePage.spec.js
+++ tests/e2e/profilePage.spec.js
@@ -1,20 +1,12 @@
 import { test, expect } from "@playwright/test";
 import { ProfilePage } from "../../pom/pages/profile.page";
-import { GaragePage } from "../../pom/pages/garage.page";
-
-const carBrand = "Porsche";
-const carModel = "Panamera";
-const randomNumber = () => Math.floor(Math.random() * 1000 + 1);
-
-test.beforeEach(async ({ page }) => {
-  await page.goto("/");
-});
 
 test.describe("Profile page test", () => {
   test("The profile image should be uploaded and updated", async ({ page }) => {
     const profilePage = new ProfilePage(page);
 
-    await test.step("opening the profile page", async () => {
+    await test.step("opening the garage page", async () => {
+      await page.goto("/");
       await expect(page).toHaveURL(/\/panel\/garage\/?$/);
     });
 
@@ -30,41 +22,6 @@ test.describe("Profile page test", () => {
       await profilePage.saveButton.click();
 
       await expect(profilePage.successPopup).toBeVisible();
-    });
-  });
-
-  test("The car and a fuel expanse should be added", async ({ page }) => {
-    const garagePage = new GaragePage(page);
-
-    await test.step("adding a car to the garage", async () => {
-      await garagePage.addCarButton.click();
-
-      await garagePage.brandDropdown.selectOption(carBrand);
-      await garagePage.modelDropdown.selectOption(carModel);
-      await garagePage.mileageInput.fill(randomNumber().toString());
-      await garagePage.addButton.click();
-
-      await expect(garagePage.carListItem.first()).toContainText(
-        `${carBrand} ${carModel}`
-      );
-    });
-    await test.step("adding the fuel expense", async () => {
-      await garagePage.addFuelExpenseButton.first().click();
-
-      const liters = randomNumber();
-      const cost = randomNumber();
-
-      await garagePage.numberOfLiters.fill(liters.toString());
-      await garagePage.totalCost.fill(cost.toString());
-
-      await garagePage.mileageInput.click();
-      await page.keyboard.press("ArrowUp");
-
-      await garagePage.addButton.click();
-
-      await expect(page.locator("tbody > tr")).toContainText(
-        `${liters}L${cost}.00 USD`
-      );
     });
   });
 });
